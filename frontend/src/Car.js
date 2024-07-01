0e6e167cf11658224cf9801cdafd322f10075c2f
uri <uri@uris-MacBook-Pro.local>
2024-07-01 10:11:04 -0700

    removed un-used import

--- frontend/src/Car.js
+++ frontend/src/Car.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { useFrame, useLoader } from "@react-three/fiber";
+// import { useFrame } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Mesh } from "three";
 
