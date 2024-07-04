8aba03c5d0dd53e7dc3d8a7f03ccd86054c2efcf
uri <uri@uris-mbp.mynetworksettings.com>
2024-07-04 12:17:24 -0700

    updated socket.io

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'http://192.128.100.27:5000' : 'http://localhost:5000';
+const URL = process.env.NODE_ENV === 'production' ? 'http://192.168.0.153:5000' : 'http://localhost:5000';
 
 export const socket = io(URL);
