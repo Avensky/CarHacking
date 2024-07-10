cf546651bd45f6b27436a7128260a84d7012e938
uri <uri@uris-mbp.mynetworksettings.com>
2024-07-10 00:34:54 -0700

    fixed the car engine simulation script

--- backend/car.js
+++ backend/car.js
@@ -10,71 +10,83 @@ var msg = {
     data: [0, 0, 0, 0, 0, 0, 0, 0]
 }
 
-var start = 1;
-var speed = 0;
-var revs = 0;
-var up = true;
-var fuel = 500;
-// var temp = 210
+const canData = {
+    start: 1,
+    speed: 0,
+    revs: 0,
+    up: true,
+    fuel: 500,
+    gear: 0,
+    index: 0,
+}
+const started = canData;
 
-setInterval(() => {
+const engine = () => {
+    const now = started;
+    // if engine is on in neutral, raise them to 1000
+    if (now.gear === 0 && (now.revs <= 0 || now.revs <= 800)) {
+        now.revs += 100;
+    }
+
+    // while gears are engaged inscrease speed
+    if (now.gear !== 0) {
+
+        // fluctuate rpms to increase from 2000rpms to 6000rpms
+        if (now.revs < 6000) {
+            now.revs += 200;
+            now.speed += 1;
+        } else if (now.revs >= 6000) {
+            if (now.gear < 6) {
+                now.revs -= 4000;
+                now.gear++;
+                now.speed += 1;
+            }
+        }
+    }
+
+    // Use an index to alternate between adding and subtracting revs, simulating an engines fluctuation
+    if (now.index % 2 === 0) {
+        now.revs += 50;
+    } else {
+        now.revs -= 50;
+    }
+
+    // reduce fuel
+    if (now.fuel > 0) {
+        now.fuel -= 1;
+    }
+
+    // run out of fuel
+    if (now.fuel <= 0) {
+        if (now.speed >= 0) {
+            now.speed -= 5;
+        }
+    }
+
+    // print results
+    console.log("speed = ", now.speed);
+    console.log("revs  = ", now.revs);
+    console.log("gear  = ", now.gear);
+    console.log("fuel  = ", now.fuel);
+    console.log("------------------");
+    now.index++;
+
+    // send data
     var out = {}
     var buff = Buffer.alloc(8)
-    if (speed < 113) {
-        speed = speed + 1
-        revs = revs + 240
-        fuel = fuel - 1
-    } else {
-        if (up) {
-            revs = revs + 100
-            up = false
-        } else {
-            revs = revs - 100
-            up = true
-        }
-        fuel = fuel - 1
-    }
 
-    if (revs > 7000) {
-        revs = -1000
-    }
-    if (revs < 2000) {
-        revs = +500
-    }
-
-    if (fuel == 0) {
-        speed = 0
-        fuel = 0
-        if (revs >= 700) {
-            revs += 100
-        }
-        if (revs < 700) {
-            revs -= 100
-        }
-    }
-
-    if (start === 1 && speed === 0) {
-        fuel = fuel - 1
-        if (revs >= 700) {
-            revs += 100
-        }
-        if (revs < 700) {
-            revs -= 100
-        }
-    }
-
-    buff.writeUIntBE(revs, 0, 4)
-    buff.writeUIntBE(speed, 4, 2)
-    buff.writeUIntBE(fuel, 6, 2)
-    // attempt to add new gauge 
-    // buff.writeUIntBE(temp, 2, 4)
-
+    buff.writeUIntBE(now.revs, 0, 4)
+    buff.writeUIntBE(now.speed, 4, 2)
+    buff.writeUIntBE(now.fuel, 6, 2)
 
     console.log(buff)
     out.id = msg.id
     out.data = buff
 
     channel.send(out)
-}, 1000)
+}
+
+// run script every 10 times per second
+setInterval(engine, 100);
 
 channel.start();
