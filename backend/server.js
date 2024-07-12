19631a2e4c0c929c1afee3e3b83a3af59130a4cc
uri <uri@uris-mbp.mynetworksettings.com>
2024-07-12 11:05:25 -0700

    added api requests

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,16 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
+// API CALLS
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.get('/api/start', ping);
+const ping = (req, res) => {
+    console.log("ping succesful");
+    res.send('ping');
+}
+
+
 server.listen(PORT, (err) => {
     if (!err) {
         console.log('server started running on: ' + PORT);
--- frontend/src/components/ConnectionManager.js
+++ frontend/src/components/ConnectionManager.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { socket } from '../socket';
 
 export function ConnectionManager() {
+
+    const base_url = process.env.NODE_ENV === "production"
+        ? "http://192.168.0.153/api/start"
+        : "http://127.0.0.1:5000/api/start"
+
     function connect() {
-        socket.connect();
+        axios.get(base_url)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+        console.log(JSON.stringify(data))
+        // socket.connect();
     }
 
     function disconnect() {
