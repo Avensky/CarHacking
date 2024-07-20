bd0f6f6b9255797d0e5e3d4c4163c24489111492
uri <uri@uris-mbp.mynetworksettings.com>
2024-07-19 22:32:45 -0700

    updated proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 let baseUrl
 
 process.env.NODE_ENV === "production"
-  ? baseUrl = "http://localhost:5000"
+  ? baseUrl = "http://127.0.0.1:5000"
   : baseUrl = "http://127.0.0.1:4000"
 
 module.exports = function (app) {
