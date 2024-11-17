170d7a1036dc770e47128246b1c3def056c92332
Avensky <urielzacarias@gmail.com>
2024-11-16 23:36:25 -0800

    removed old code

--- frontend/src/ui/console/index.tsx
+++ frontend/src/ui/console/index.tsx
@@ -4,7 +4,6 @@ import { MyForm } from './Terminal/MyForm'
 import { Log } from './log/Log'
 
 export function Console(props: { isConnected: boolean; cmdEvents: any }): JSX.Element {
-
   return (
     <div className="console ">
       <Log events={props.cmdEvents} />
@@ -15,17 +14,17 @@ export function Console(props: { isConnected: boolean; cmdEvents: any }): JSX.El
             <MyForm />
             {props.isConnected === true ? (
               <>
-                <Button reload={false} url="/api/start" name="Start Sim" abort={false}/>
-                <Button reload={false} url="/api/hack" name="Hack Car" abort={false}/>
-                <Button reload={false} url="/api/reload" name="Reload Node" abort={false}/>
-                <Button reload={true} url="" name="Reload UI" abort={false}/>
-                <Button reload={false} url="/api/abort" name="Abort" abort={true}/>
+                <Button reload={false} url="/api/start" name="Start Sim" />
+                <Button reload={false} url="/api/hack" name="Hack Car" />
+                <Button reload={false} url="/api/reload" name="Reload Node" />
+                <Button reload={true} url="" name="Reload UI" />
+                <Button reload={false} url="/api/abort" name="Abort" />
                 {/* <Button reload={false} url='/api/ping' name="Ping" /> */}
               </>
             ) : (
               <>
-                <Button reload={false} url="/api/reload" name="Reload Node" abort={false}/>
-                <Button reload={true} url="" name="Refresh UI" abort={false}/>
+                <Button reload={false} url="/api/reload" name="Reload Node" />
+                <Button reload={true} url="" name="Refresh UI" />
               </>
             )}
           </div>
