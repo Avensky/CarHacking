e82e7f0ef783a1c3eb4a4c2345098d133327e33f
uri <uri@macbookpro.mynetworksettings.com>
2025-06-14 04:18:30 -0700

    Removed extra large map
    Former-commit-id: 7d287769de20aebdced989a45461200e72e65ebc

--- frontend/src/models/environments/VirtualCity.tsx
+++
@@ -1,9 +0,0 @@
-import { Environment, useGLTF } from "@react-three/drei"
-
-export default function VirtualCity(props) {
-    const { scene } = useGLTF('/models/virtualCity.glb')
-    return <group scale={8}>
-        <Environment preset="night" />
-        <primitive object={scene} {...props} />;
-    </group>
-}
