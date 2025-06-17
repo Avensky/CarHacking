968e1281d304a87bd0fccf7fdf3f4b9097fd62d3
uri <uri@uris-MacBook-Pro.local>
2025-06-17 09:45:25 -0700

    added virtual can

--- socket.js
+++ socket.js
@@ -1,11 +1,26 @@
 const { createVehicle, stepWorld, updateVehicleInputs } = require('./physics');
 
+let channel
+if (process.env.NODE_ENV === "production") {
+  const can = require("socketcan");
+  // sudo apt-get install can-utils
+  // sudo modprobe vcan
+  // sudo ip link add dev vcan0 type vcan
+  // sudo ip link set up vcan0
+
+  channel = can.createRawChannel("vcan0", true);
+  channel.addListener("onMessage", msg => {
+    console.log("Received CAN:", msg);
+  });
+  channel.start();
+
+}
 const inputs = {};
 
 function setupSocketIO(io) {
   io.on('connection', (socket) => {
     console.log('Client connected:', socket.id);
-     
+
     inputs[socket.id] =
     {
       forward: false,
@@ -39,6 +54,14 @@ function setupSocketIO(io) {
     });
   });
 
+  // Add Meaningful CAN ID Mapping
+  // You can differentiate between message types using different CAN IDs:
+  // ID	Purpose
+  // 0x101	Speed/Revs
+  // 0x102	Fuel + Gear
+  // 0x103	Blinker State
+  // 0x104	Diagnostics
+
   setInterval(() => {
     Object.entries(inputs).forEach(([id, control]) => {
       updateVehicleInputs(id, control);
@@ -47,6 +70,42 @@ function setupSocketIO(io) {
     const snapshots = stepWorld();
     Object.entries(snapshots).forEach(([id, data]) => {
       io.to(id).emit('physicsUpdate', data);
+
+      if (channel) {
+        // default values
+        let msg = {
+          id: 0x123,
+          data: Buffer.from([0xAA, 0xBB, 0xCC])
+        };
+
+        const canData = {
+          speed: 0,
+          revs: 0,
+          up: true,
+          fuel: 500,
+          gear: 1,
+          index: 0,
+        }
+
+        // send data 
+        var out = {}
+        var buff = Buffer.alloc(8)
+
+        buff.writeUIntBE(canData.revs, 0, 4)
+        buff.writeUIntBE(data.speed, 4, 2)
+        buff.writeUIntBE(canData.fuel, 6, 2)
+
+        console.log('physics', data)
+        console.log('can', buff)
+        out.id = msg.id
+        out.data = buff
+        channel.send(out);
+      }
+
+      io.to(id).emit('canData', {
+        id: out.id,
+        data: Array.from(buff), // easier to send over WebSocket
+      });
     });
   }, 1000 / 60);
 }
